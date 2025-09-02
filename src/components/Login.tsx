a2484d6ad73d8795938508dee12683ac7a5cb367
jodaripaa <142604612+jodaripaa@users.noreply.github.com>
2025-09-02 17:04:03 -0500

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,19 +2,19 @@ import React from "react";
 
 const Login: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-[#0D1215] via-[#3C2865] via-[#653792] via-[#475AA5] to-[#2497B4]">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b* from-[#0D1215] via-[#3C2865] via-[#653792] via-[#475AA5] to-[#2497B4]">
       <div className="flex w-[900px] h-[500px] rounded-2xl overflow-hidden shadow-2xl bg-white">
         {/* Lado izquierdo con imagen */}
         <div
           className="flex-1"
           style={{
-            backgroundImage: "url('/login.png')", // 👈 coloca tu imagen en /public/login.png
+            backgroundImage: "url('/login.png')", // 
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
         ></div>
 
-        {/* Lado derecho con formulario */}
+
         <div className="flex-1 flex flex-col justify-center items-center p-10">
           <h1 className="text-3xl font-bold mb-8">Login</h1>
 
@@ -56,7 +56,7 @@ const Login: React.FC = () => {
               type="submit"
               className="w-full p-3 bg-black text-white rounded-xl hover:bg-gray-800 transition"
             >
-              Ingresar
+
             </button>
           </form>
         </div>
